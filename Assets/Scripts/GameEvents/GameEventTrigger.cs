2e46a231554af9f8f517322629886ee943d48e6e
Samuele-Angeletti <104771865+Samuele-Angeletti@users.noreply.github.com>
2025-10-11 16:36:27 +0200

    Update GameEventTrigger.cs

--- Assets/Scripts/GameEvents/GameEventTrigger.cs
+++ Assets/Scripts/GameEvents/GameEventTrigger.cs
@@ -7,14 +7,26 @@ public class GameEventTrigger : MonoBehaviour
     private void Awake()
     {
         spriteRenderer = GetComponentInChildren<SpriteRenderer>();
-        spriteRenderer.sprite = gameEvent.SpriteIcon;
+
+        if (gameEvent.ShowIcon)
+            spriteRenderer.sprite = gameEvent.SpriteIcon;
+
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.TryGetComponent<Player>(out _))
         {
+            if (gameEvent.GameEventEffect == EGameEventEffect.BombDrop)
+            {
+                Instantiate(gameEvent.BombPrefab, transform.position, Quaternion.identity);
+                gameObject.SetActive(false);
+                return;
+            }
+
             Publisher.Publish(new OpenEventUIMessage(gameEvent));
+            gameObject.SetActive(false);
         }
     }
 }
