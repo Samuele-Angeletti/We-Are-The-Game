043613cfa4015948a5b851013c8a6407c1a73d8d
xanakoby <scottmoreno6@gmail.com>
2025-10-11 16:56:44 +0200

    DialogueFruitRandom

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -6,32 +6,40 @@ using UnityEngine.UI;
 public class UIManager : Singleton<UIManager>
 {
     [Header("Player/NPC Dialogue")]
-    public GameObject playerDialogueContainer;
-    public TMP_Text playerText;
-    public Image NPCImage;
-    public GameObject NPCDialogueContainer;
-    public TMP_Text NPCText;
+    [SerializeField] GameObject playerDialogueContainer;
+    [SerializeField] TMP_Text playerText;
+    [SerializeField] Image NPCImage;
+    [SerializeField] GameObject NPCDialogueContainer;
+    [SerializeField] TMP_Text NPCText;
 
     [Header("Total Cities")]
     public GameObject[] totalCities;
 
     [Header("Current City")]
-    public GameObject currentCityOptionsPanel;
-    public Slider currentCityHealthBar;
-    public Slider currentCityMedicinesBar;
-    public TMP_Text currentCityHealthText;
-    public TMP_Text currentCityMedicinesText;
-    public TMP_Text currentCityDialogueText;
+    [SerializeField] GameObject currentCityOptionsPanel;
+    [SerializeField] Slider currentCityHealthBar;
+    [SerializeField] Slider currentCityMedicinesBar;
+    [SerializeField] TMP_Text currentCityHealthText;
+    [SerializeField] TMP_Text currentCityMedicinesText;
+    [SerializeField] TMP_Text currentCityDialogueText;
 
     [Header("PopUp")]
-    public GameObject popUpPanel;
-    public TMP_Text popUpText;
+    [SerializeField] GameObject popUpPanel;
+    [SerializeField] TMP_Text popUpText;
+
+    [Header("Dialogue Vars")]
+    [SerializeField] int minDialogueLetters = 10;
+    [SerializeField] int maxDialogueLetters = 15;
 
     public override void Awake()
     {
         base.Awake();
     }
-
+    private void Start()
+    {
+        playerText.text = RandomDialogueLetters();
+        NPCText.text = RandomDialogueLetters();
+    }
     public void ShowPopup(string _dialogue)
     {
         popUpPanel.SetActive(true);
@@ -49,6 +57,9 @@ public class UIManager : Singleton<UIManager>
         NPCImage.gameObject.SetActive(true);
 
         currentCityOptionsPanel.SetActive(true);
+
+        playerText.text = RandomDialogueLetters();
+        NPCText.text = RandomDialogueLetters();
     }
     public void HideCityPanelOptions()
     {
@@ -57,5 +68,43 @@ public class UIManager : Singleton<UIManager>
         NPCImage.gameObject.SetActive(false);
 
         currentCityOptionsPanel.SetActive(false);
+    }
+    public string RandomDialogueLetters()
+    {
+        int maxChars = Random.Range(minDialogueLetters, maxDialogueLetters + 1);
+
+        // Se maxChars è minore di 1, restituiamo stringa vuota
+        if (maxChars < 1)
+            return "";
+
+        // Definisce i caratteri possibili (puoi aggiungere numeri o simboli se vuoi)
+        const string letters = "abcdefghijklmnopqrstuvwxyz";
+
+        System.Text.StringBuilder sb = new System.Text.StringBuilder();
+        int currentLength = 0;
+
+        while (currentLength < maxChars)
+        {
+            // Decidi casualmente la lunghezza della parola (da 1 a 8 lettere, ma non oltre maxChars)
+            int wordLength = Random.Range(1, Mathf.Min(8, maxChars - currentLength) + 1);
+
+            // Aggiungi lettere
+            for (int i = 0; i < wordLength && currentLength < maxChars; i++)
+            {
+                char randomLetter = letters[Random.Range(0, letters.Length)];
+                sb.Append(randomLetter);
+                currentLength++;
+            }
+
+            // Aggiungi spazio casualmente, solo se non stai per superare maxChars
+            if (currentLength < maxChars && Random.value > 0.3f) // 70% di probabilità di aggiungere spazio
+            {
+                sb.Append(' ');
+                currentLength++;
+            }
+        }
+
+        // Rimuove eventuale spazio finale
+        return sb.ToString().TrimEnd();
     }
 }
