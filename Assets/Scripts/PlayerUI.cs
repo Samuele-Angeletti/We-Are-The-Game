1d0ae83f4d2e4af522073d38eb76f8f76619b61a
DavideDalDosso <95852180+DavideDalDosso@users.noreply.github.com>
2025-10-11 15:45:50 +0200

    Stamina Bar Tweening

--- Assets/Scripts/PlayerUI.cs
+++ Assets/Scripts/PlayerUI.cs
@@ -10,19 +10,44 @@ public class PlayerUI : MonoBehaviour
     public TMP_Text medicineCount;
     public Slider staminaSlider;
     public Image staminaFill;
+    public CanvasGroup staminaGroup;
+    public Transform staminaTransitionTarget;
+    private Vector2 staminaTransitionOriginal;
+    [Header("State")]
+    public bool staminaVisible;
     [Header("Visual")]
     [Range(0f, 1f)]
     public float staminaLowThreshold;
     public Color staminaNormalColor;
     public Color staminaLowColor;
+    public float staminaTransitionTime;
 
     void Start()
     {
-        
+        staminaTransitionOriginal = staminaSlider.transform.localPosition;
+
+        staminaVisible = false;
+        staminaSlider.gameObject.transform.localPosition = staminaTransitionTarget.localPosition;
+        staminaGroup.alpha = 0;
     }
 
     void Update()
     {
+        if (player.stamina >= player.staminaCap && staminaVisible == true)
+        {
+            staminaVisible = false;
+
+            LeanTween.moveLocal(staminaSlider.gameObject, staminaTransitionTarget.localPosition, staminaTransitionTime).setEaseInCirc();
+            LeanTween.alphaCanvas(staminaGroup, 0, staminaTransitionTime).setEaseInCirc();
+        }
+        if (player.stamina < player.staminaCap && staminaVisible == false)
+        {
+            staminaVisible = true;
+
+            LeanTween.moveLocal(staminaSlider.gameObject, staminaTransitionOriginal, staminaTransitionTime).setEaseOutCirc();
+            LeanTween.alphaCanvas(staminaGroup, 1, staminaTransitionTime).setEaseOutCirc();
+        }
+
         medicineCount.text = player.medicine.ToString();
 
         var staminaValue = player.stamina / player.staminaCap;
@@ -35,5 +60,15 @@ public class PlayerUI : MonoBehaviour
         {
             staminaFill.color = staminaLowColor;
         }
+    }
+
+    private void SetImageTransparency(float value)
+    {
+        staminaFill.color = ChangeTransparency(staminaFill.color, value);
+    }
+
+    private Color ChangeTransparency(Color color, float value)
+    {
+        return new Color(color.r, color.b, color.g, value);
     }
 }
